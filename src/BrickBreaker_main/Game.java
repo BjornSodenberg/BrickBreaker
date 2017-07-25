88ab248b4676844be557337072c1a24056dc8632
Bjorn <garonini1@gmail.com>
2017-07-25 19:24:43 +0700

    Added move for paddle and ball. Fixed timer.

--- src/BrickBreaker_main/Game.java
+++ src/BrickBreaker_main/Game.java
@@ -2,11 +2,12 @@ package BrickBreaker_main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import javax.swing.Timer;
 
 import javax.swing.JPanel;
 
@@ -33,8 +34,8 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		addKeyListener(this);
 		setFocusable(true);
 		setFocusTraversalKeysEnabled(false);
-//		timer = new Timer(delay,this);
-//		timer.start();
+		timer = new Timer(delay,this);
+		timer.start();
 		
 	}
 	
@@ -56,14 +57,36 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 		
 		//ball
 		g.setColor(Color.red);
-		g.fillRect(ballposX, ballposY, 20, 20);
+		g.fillOval(ballposX, ballposY, 20, 20);
+		
+		g.dispose();
 		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
+		timer.start();
 		
+		if(play){
+			if(new Rectangle(ballposX,ballposY, 20, 20).intersects(
+					new Rectangle(playerX, 550, 100,8))) {
+				ballYdir = -ballYdir;
+			}
+			ballposX += ballXdir;
+			ballposY += ballYdir;
+			if(ballposX < 0) {
+				ballXdir = -ballXdir;
+			}
+			if(ballposY < 0) {
+				ballYdir = -ballYdir;
+			}
+			if(ballposX > 670) {
+				ballXdir = -ballXdir;
+			}
+		}
+		
+		repaint();
 	}
 
 	@Override
