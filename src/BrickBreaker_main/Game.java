36821dba48cbf1efeccf56cdd1f327e5c0f177ef
Bjorn <garonini1@gmail.com>
2017-07-25 18:45:10 +0700

    init of var

---
+++ src/BrickBreaker_main/Game.java
@@ -0,0 +1,58 @@
+package BrickBreaker_main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements KeyListener, ActionListener {
+	
+	private boolean play = false;
+	private int score = 0;
+	
+	private int totalBricks = 21;
+	
+	private Timer time;
+	private int delay = 8;
+	
+	private int playerX = 310;
+	
+	private int ballposX = 120;
+	private int ballposY = 350;
+	private int ballXdir = -1;
+	private int ballYdir = -2;
+	
+	
+	public Game()
+	{
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
