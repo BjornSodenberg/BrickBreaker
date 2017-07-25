777452d667da38df682962814dc04d9c24207654
Bjorn <garonini1@gmail.com>
2017-07-25 18:54:25 +0700

    init game objects

--- src/BrickBreaker_main/Game.java
+++ src/BrickBreaker_main/Game.java
@@ -1,5 +1,7 @@
 package BrickBreaker_main;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -15,7 +17,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	
 	private int totalBricks = 21;
 	
-	private Timer time;
+	private Timer timer;
 	private int delay = 8;
 	
 	private int playerX = 310;
@@ -28,6 +30,33 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	
 	public Game()
 	{
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		timer = new Timer(delay,this);
+		timer.start();
+		
+	}
+	
+	public void Draw(Graphics g)
+	{
+		//bg
+		g.setColor(Color.black);
+		g.fillRect(1, 1, 692, 592);
+		//borders
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(691, 0, 3, 592);
+		
+		//paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		
+		//ball
+		g.setColor(Color.red);
+		g.fillRect(ballposX, ballposY, 20, 20);
 		
 	}
 	
